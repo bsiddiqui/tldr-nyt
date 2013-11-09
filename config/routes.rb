cd546c056cbd3e1faa9418138ac640ad6aa0ba1e
Basil Siddiqui <basil.siddiqui@gmail.com>
2013-11-09 10:06:19 -0500

    added endpoint for updating

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Tldrnyt::Application.routes.draw do
   get "recent", to: "articles#recent"
+  get "update", to: "articles#get_recent"
   resources :articles, only: [:index]
 
   get 'partials/*partial' => 'partials#partial'
